0ca72d6f36da40c7a47d06e1e5772ff999a0996c
Buggyes <matheusthja1@gmail.com>
2023-08-07 13:51:29 -0300

    Receita colocada dentro da pasta Ryori
    Receita de takoyaki adicionada no projeto.

---
+++ Ryori/js/recipes/returnButton.js
@@ -0,0 +1,5 @@
+var button = document.getElementsByClassName("returnButton")
+
+function returnToMainPage() {
+    window.location = "receitas.html"; //<--------inserir o link da pagina de receitas aqui
+}
